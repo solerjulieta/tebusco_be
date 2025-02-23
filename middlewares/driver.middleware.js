0d92ce87071ea554888ba07ea55a26587fed9eb9
Julieta Soler <julieta_soler@hotmail.com>
2025-02-23 17:42:49 -0300

    Se corrige driver.middleware

--- middlewares/driver.middleware.js
+++ middlewares/driver.middleware.js
@@ -9,9 +9,9 @@ async function validateRegister(req, res, next)
     if (req.uploadType === 'driverAuth' && !req.file) {
         return res.status(400).json({ error: { msg: 'La habilitación es requerida para el registro.' } });
     }
-    /*
+    
     const authorizationFile = req.file
-
+    /*
     if (!authorizationFile) {
         return res.status(400).json({ error: { msg: 'La habilitación es obligatoria' } })
     }*/
