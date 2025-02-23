560255ee67f97d0c35335008cde75b91d1dae1d0
Julieta Soler <julieta_soler@hotmail.com>
2025-02-22 21:23:48 -0300

    Se corrige APP.JS

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(cors({
     origin: 'https://tebusco.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'auth-token']
 }))
 //app.use('/icons', express.static(path.join(__dirname, 'public/icons')))
 
